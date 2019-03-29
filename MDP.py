65bf97f1ddb1ed815b007a402726cefaa60b6854
adamlkl <kleong@tcd.ie>
2019-03-29 17:28:21 +0000

    halfway there

--- MDP.py
+++ MDP.py
@@ -13,11 +13,48 @@ class MDP:
     
     # TODO
     ''' 
-        -add dead state 
-        -reconstruct state tables 
-        -change functions 
-    '''
+        - add dead state 
+        - reconstruct state tables 
+        - change functions 
+        - make dynamic 
+    '''    
+    def __init__(self,gamma):
+        self.gamma = gamma
+        self.qvalues = {'exercise':{'fit':[],'unfit':[],'dead':[]},
+                        'relax':{'fit':[],'unfit':[],'dead':[]}
+                        }
+        
+    def calculateQ(self, state, action, n):
+        if(len(q)<n):
+            if n==0:
+                    q = const.baseExerciseTable[action][state]['fit'][0]*const.baseExerciseTable[action][state]['fit'][1]+const.baseExerciseTable[action][state]['unfit'][0]*const.baseExerciseTable[action][state]['ununfit'][1]
+            else: 
+                    q = q + self.gamma*calculateQ(state, action, n-1)
+            self.qvalues[n]=q
+            return q
+        else:
+            return qvalues[action][state][n]
     
+    def v(self, state, n):
+        return max(calculateQ(state,'exercise',n),calculateQ(state,'relax',n))
+    
+    def getReward(self):
+    
+    def getProbability(self):
+        
+def main():
+    inputs = sys.argv
+    if(len(inputs)!=3):
+        print('Input error!!!')
+    else:
+        mdp = MDP(inputs[3])
+        print(mdp.calculateQ(inputs[1],'exercise',inputs[2]))
+        print(mdp.calculateQ(inputs[1],'relax',inputs[2]))
+    
+if __name__ == '__main__':
+    main()
+    
+'''
     class StateTable:
             
         def __init__(self, eP, rP, eR, eR, condition):
@@ -35,22 +72,4 @@ class MDP:
         self.fitState = State(const.exeState, relState, exeReward, relReward, 'fit')
         self.unfitState = State(const.exeState, relState, exeReward, relReward, 'unfit')
         self.startState = fitState if startState == 'fit' else self.unfitState
-        
-    def q(self):
-        
-    def v(self):
-        
-    def getReward(self):
-    
-    def getProbability(self):
-        
-        
-def main():
-    inputs = sys.argv
-    if(len(inputs)!=3):
-        print('Input error!!!')
-    else:
-        mdp = MDP(inputs[1],inputs[2],inputs[3])
-    
-if __name__ == '__main__':
-    main()
+'''
--- constants.py
+++ constants.py
@@ -18,14 +18,14 @@ exeReward = [[8,8],[0,0]]
 relReward = [[10,10],[5,5]]
 '''
 
-baseRelaxTable = {
-              'fit':{'fit':[0.693,10], 'unfit':[0.297,10], 'dead':[0.01,0]},
-              'unfit':{'fit':[0,5], 'unfit':[0.99,5], 'dead':[0.01,0]},
-              'dead':{'fit':[0,0], 'unfit':[0,0], 'dead':[1,0]}
-              }
-
-baseExerciseTable = {
-              'fit':{'fit':[0.891,8], 'unfit':[0.09,8], 'dead':[0.1,0]},
-              'unfit':{'fit':[0.18,0], 'unfit':[0.72,0], 'dead':[0.1,0]},
-              'dead':{'fit':[0,0], 'unfit':[0,0], 'dead':[1,0]}
-              }
+baseTable = {'relax':{
+                      'fit':{'fit':[0.693,10], 'unfit':[0.297,10], 'dead':[0.01,0]},
+                      'unfit':{'fit':[0,5], 'unfit':[0.99,5], 'dead':[0.01,0]},
+                      'dead':{'fit':[0,0], 'unfit':[0,0], 'dead':[1,0]}
+                     },
+            'exercise':{
+                          'fit':{'fit':[0.891,8], 'unfit':[0.09,8], 'dead':[0.1,0]},
+                          'unfit':{'fit':[0.18,0], 'unfit':[0.72,0], 'dead':[0.1,0]},
+                          'dead':{'fit':[0,0], 'unfit':[0,0], 'dead':[1,0]}
+                      }
+            }
