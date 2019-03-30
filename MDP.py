3c914715d474fb15d183c4e8b370ba49fbd97c03
adamlkl <kleong@tcd.ie>
2019-03-30 21:20:51 +0000

    dynamic done

--- MDP.py
+++ MDP.py
@@ -20,23 +20,28 @@ class MDP:
     '''    
     def __init__(self,gamma):
         self.gamma = gamma
-        self.qvalues = {'exercise':{'fit':[],'unfit':[],'dead':[]},
-                        'relax':{'fit':[],'unfit':[],'dead':[]}
+        self.qvalues = {'exercise':{'fit':{},'unfit':{},'dead':{}},
+                        'relax':{'fit':{},'unfit':{},'dead':{}}
                         }
         self.nV = {}
         
     def calculateQ(self, state, action, n):
-        #if(len(self.qvalues[action][state])<n):
-        q = 0
-        if n==0:
-            for s in const.STATE:
-                q = q + const.baseTable[action][state][s][0]*const.baseTable[action][state][s][1]
-                
+        if n not in self.qvalues[action][state]:
+            q = 0
+            if n==0:
+                for s in const.STATE:
+                    q = q + const.baseTable[action][state][s][0]*const.baseTable[action][state][s][1]
+                    self.qvalues[action][state][n]=q
+            else: 
+                q = self.calculateQ(state, action, 0) + self.gamma*(const.baseTable[action][state]['fit'][0]*self.getV('fit', n-1)+const.baseTable[action][state]['unfit'][0]*self.getV('unfit', n-1))
+                self.qvalues[action][state][n]=q
+            return q
+        
         else: 
-            q = self.calculateQ(state, action, 0) + self.gamma*(const.baseTable[action][state]['fit'][0]*self.getV('fit', n-1)+const.baseTable[action][state]['unfit'][0]*self.getV('unfit', n-1))
-            #self.qvalues[action][state][n]=q
+            return self.qvalues[action][state][n]
         #return self.qvalues[action][state][n]
-        return q
+        #print("%d q: %.15f" %(n,self.qvalues[action][state][n]))
+            
     
     def getV(self, state, n):
         #if n<len(self.nV):
@@ -47,6 +52,11 @@ class MDP:
     def getsth(self, action, state):
         return const.baseTable[action][state]['fit'][0]
     
+    def calculateAllQ(self, state, action, n):
+        for i in range(n):
+            q = self.calculateQ(state, action, i)
+            print 'n=%d %s: %.15f' %(i, action, q)
+        
 def main():
     inputs = sys.argv
     if(len(inputs)!=4):
@@ -57,8 +67,10 @@ def main():
         gamma = float(inputs[3])
         
         mdp = MDP(gamma)
-        print "1: %.15f" %(mdp.calculateQ(initial_state,'exercise',n))
-        print "2: %.15f" %(mdp.calculateQ(initial_state,'relax',n))
+        # print "1: %.15f" %(mdp.calculateQ(initial_state,'exercise',n))
+        # print "2: %.15f" %(mdp.calculateQ(initial_state,'relax',n))
+        mdp.calculateAllQ(initial_state,'exercise',n)
+        mdp.calculateAllQ(initial_state,'relax',n)
     
 if __name__ == '__main__':
     main()
